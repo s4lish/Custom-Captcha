55a19549f49fd3afeb8f9d50ab3366c9166e6d51
s4lish <s4lish@gmail.com>
2023-05-04 11:21:24 +0330

    Add project files.

---
+++ I am Not Robot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace I_am_Not_Robot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
